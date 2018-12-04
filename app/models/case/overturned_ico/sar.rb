8a88d38030f8a221c0147622ed8435f129d45747
beccapearce <rebecca.pearce@digital.justice.gov.uk>
2018-12-04 13:53:13 +0000

    CT-2022 move timeliness setter to concern

--- app/models/case/overturned_ico/sar.rb
+++ app/models/case/overturned_ico/sar.rb
@@ -1,5 +1,7 @@
 class Case::OverturnedICO::SAR < Case::OverturnedICO::Base
 
+  include DraftTimeliness::ProgressedForClearance
+
   delegate :message, to: :original_case
 
   has_paper_trail only: [
